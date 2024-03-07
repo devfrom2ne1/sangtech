b61b9780a068212de878bae2cb10d28451d775e9
devfrom2ne1 <124670711+devfrom2ne1@users.noreply.github.com>
2024-03-07 20:05:12 +0900

    set frontend using react

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch('/api/mycard')
+            .then(response => response.text())
+            .then(message => {
+                setMessage(message);
+            });
+    },[])
+    return (
+        <div className="app">
+            <header className="header">
+                <h1>형빈이와 함께하는 즐거운 상테크</h1>
+            </header>
+            <main className="main" style={{ height: '85vh' }}>
+                <section className="content">
+                    {/* 여기에 컨텐츠를 추가하세요 */}
+                    <p>여기에 내용이 쫚쫙 들어갈 예정!</p>
+                    api통신내역 : {message}
+                </section>
+            </main>
+            <footer className="footer">
+                <nav className="navbar">
+                    <a href="#" className="nav-link">카드선택</a>
+                    <a href="#" className="nav-link">구입기록</a>
+                    <a href="#" className="nav-link">나의통계</a>
+                    <a href="#" className="nav-link">카드등록</a>
+                </nav>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/main/java/com/calc/sangtech/controller/ApiController.java
@@ -0,0 +1,14 @@
+package com.calc.sangtech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping("/api/mycard")
+    public String Mycard() {
+        return "카드 등록";
+    }
+}
