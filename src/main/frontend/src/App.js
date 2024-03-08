f576d0f3ac7794685b6f7af9d444071f6aa4958d
devfrom2ne1 <124670711+devfrom2ne1@users.noreply.github.com>
2024-03-08 22:17:23 +0900

    +버튼 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,9 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-
     const [message, setMessage] = useState("");
 
     useEffect(() => {
@@ -12,7 +11,8 @@ function App() {
             .then(message => {
                 setMessage(message);
             });
-    },[])
+    }, []);
+
     return (
         <div className="app">
             <header className="header">
@@ -23,8 +23,12 @@ function App() {
                     {/* 여기에 컨텐츠를 추가하세요 */}
                     <p>여기에 내용이 쫚쫙 들어갈 예정!</p>
                     api통신내역 : {message}
+                    아니 근데 바뀐거야 만거야?
                 </section>
             </main>
+            <div className="floating-button">
+                <button className="add-button">+</button>
+            </div>
             <footer className="footer">
                 <nav className="navbar">
                     <a href="#" className="nav-link">카드선택</a>
--- src/main/java/com/calc/sangtech/controller/ApiController.java
+++ src/main/java/com/calc/sangtech/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.calc.sangtech.controller;
 
+import com.calc.sangtech.utils.GetDayUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,4 +12,12 @@ public class ApiController {
     public String Mycard() {
         return "카드 등록";
     }
+
+    @GetMapping("/api/calendar")
+    public String Calendar() {
+        //GetDayUtil getDayUtil = new GetDayUtil();
+        //getDayUtil.util();
+        return "캘린더";
+    }
+
 }
